db5a914ad823a7245c0099dfd5b70ddef45264bf
romanych2021 <romanych_2021@list.ru>
2020-10-11 05:17:44 +1100

    Commit

--- src/main/java/com/testpreauthorize/config/RootConfig.java
+++ src/main/java/com/testpreauthorize/config/RootConfig.java
@@ -3,10 +3,11 @@ package com.testpreauthorize.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.testpreauthorize.security")
-public class RootConfig {
+public class RootConfig implements WebMvcConfigurer {
 
 }

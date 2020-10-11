bd6e066e4aec4f3ce9a42cec40ab79980c9d72e2
romanych2021 <romanych_2021@list.ru>
2020-10-11 16:59:44 +1100

    Commit

--- src/main/java/com/testpreauthorize/config/RootConfig.java
+++ src/main/java/com/testpreauthorize/config/RootConfig.java
@@ -2,7 +2,6 @@ package com.testpreauthorize.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
